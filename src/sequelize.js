a953b1bf02d654b8c199d071010764e0c3ab6e69
Ian King <ian.antking@gmail.com>
2020-04-27 18:41:56 +0100

    change DB_DATABASE to DB_NAME

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 const ArtistModel = require('./models/artist');
 
-const { DB_DATABASE, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   port: 3306,
   dialect: 'mysql',
