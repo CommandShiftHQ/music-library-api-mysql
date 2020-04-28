2e222bc72c5f336826d9cf77291ff7cc75eb5755
Ian King <ian.antking@gmail.com>
2020-04-28 09:37:16 +0100

    add album tests

---
+++ __tests__/albums.test.js
@@ -0,0 +1,77 @@
+const request = require('supertest');
+const app = require('../src/app');
+const { Artist, Album } = require('../src/sequelize');
+
+describe('/albums', () => {
+  let artist;
+
+  beforeEach(done => {
+    Artist.destroy({
+      where: {},
+    }).then(() => {
+      Artist.create(
+        {
+          name: 'Tame Impala',
+          genre: 'Rock',
+        },
+        (_, document) => {
+          artist = document;
+          done();
+        },
+      );
+    });
+  });
+
+  afterAll(done => {
+    Artist.destroy({
+      where: {},
+    })
+      .then(() =>
+        Album.destroy({
+          where: {},
+        }),
+      )
+      .then(() => Artist.sequelize.close().then(() => done()));
+  });
+
+  describe('POST /artists/:artistId/albums', () => {
+    xit('creates a new album for a given artist', done => {
+      request(app)
+        .post(`/artists/${artist.id}/albums`)
+        .send({
+          name: 'InnerSpeaker',
+          year: 2010,
+        })
+        .then(res => {
+          expect(res.status).toBe(201);
+
+          Album.find({ where: { id: artist.id } }, (err, album) => {
+            expect(err).toBe(null);
+            expect(album.name).toBe('InnerSpeaker');
+            expect(album.year).toBe(2010);
+            expect(album.artist).toEqual(artist.id);
+            done();
+          });
+        });
+    });
+
+    xit('returns a 404 and does not create an album if the artist does not exist', done => {
+      request(app)
+        .post('/artists/1234/albums')
+        .send({
+          name: 'InnerSpeaker',
+          year: 2010,
+        })
+        .then(res => {
+          expect(res.status).toBe(404);
+          expect(res.body.error).toBe('The artist could not be found.');
+
+          Album.find({}, (err, albums) => {
+            expect(err).toBe(null);
+            expect(albums.length).toBe(0);
+            done();
+          });
+        });
+    });
+  });
+});
