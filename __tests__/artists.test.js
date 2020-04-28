8b28454407381fedc46e3c0da9eb621c03d61d52
Ian King <ian.antking@gmail.com>
2020-04-28 09:30:42 +0100

    comment out first artist test

--- __tests__/artists.test.js
+++ __tests__/artists.test.js
@@ -16,7 +16,7 @@ describe('/artists', () => {
   });
 
   describe('POST /artists', () => {
-    it('creates a new artist in the database', done => {
+    xit('creates a new artist in the database', done => {
       request(app)
         .post('/artists')
         .send({
