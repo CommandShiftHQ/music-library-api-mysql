17269abf415d643c4ff214aee26ff25eeb725a13
Ian King <ian.antking@gmail.com>
2020-04-28 21:31:31 +0100

    define album artist relationship

--- src/models/album.js
+++ src/models/album.js
@@ -13,5 +13,8 @@ module.exports = (sequelize, DataTypes) => {
   };
 
   const Album = sequelize.define('Album', schema);
+  Album.associate = models => {
+    Album.belongsTo(models.Artist, { foreignKey: 'artist', as: 'artists' });
+  };
   return Album;
 };
