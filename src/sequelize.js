ea7485073ff80eb8fd84cac7515837e411e6575c
Ian King <ian.antking@gmail.com>
2020-04-28 15:02:03 +0100

    wrap database setup in function

--- src/sequelize.js
+++ src/sequelize.js
@@ -3,16 +3,19 @@ const ArtistModel = require('./models/artist');
 
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  port: 3306,
-  dialect: 'mysql',
-});
+const setupDatabase = () => {
+  const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    port: 3306,
+    dialect: 'mysql',
+  });
 
-const Artist = ArtistModel(sequelize, Sequelize);
+  const Artist = ArtistModel(sequelize, Sequelize);
 
-sequelize.sync();
-
-module.exports = {
-  Artist,
+  sequelize.sync();
+  return {
+    Artist,
+  };
 };
+
+module.exports = setupDatabase();
