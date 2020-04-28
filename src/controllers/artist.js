6f53fc5d67587cc1d550efc1c8be83f993a0e7da
Ian King <ian.antking@gmail.com>
2020-04-28 15:32:19 +0100

    get all artists

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -3,3 +3,9 @@ const { Artist } = require('../sequelize');
 exports.createArtist = (req, res) => {
   Artist.create(req.body).then(user => res.status(201).json(user));
 };
+
+exports.getArtists = (req, res) => {
+  Artist.findAll().then(artists => {
+    res.status(200).json(artists);
+  });
+};
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -3,6 +3,9 @@ const artistController = require('../controllers/artist');
 
 const router = express.Router();
 
-router.route('/').post(artistController.createArtist);
+router
+  .route('/')
+  .post(artistController.createArtist)
+  .get(artistController.getArtists);
 
 module.exports = router;
