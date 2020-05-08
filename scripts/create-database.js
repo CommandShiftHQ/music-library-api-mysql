919d24e7a3fe864e32ccda94ecb938cee55579df
Ian King <ian.antking@gmail.com>
2020-05-09 00:25:50 +0100

    add test argument so script can be re-used for npm start

--- scripts/create-database.js
+++ scripts/create-database.js
@@ -2,8 +2,12 @@
 const mysql = require('mysql2');
 const path = require('path');
 
+const args = process.argv.slice(2)[0];
+
+const envFile = args === 'test' ? '../.env.test' : '../.env';
+
 require('dotenv').config({
-  path: path.join(__dirname, '../.env.test'),
+  path: path.join(__dirname, envFile),
 });
 
 const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
