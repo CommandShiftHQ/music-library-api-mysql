39b019ef91022fd9f8197d82e7cd6ba9665f47fc
Ian King <36984875+ian-antking@users.noreply.github.com>
2020-05-11 21:35:36 +0100

    comment out other album and song models

--- src/sequelize.js
+++ src/sequelize.js
@@ -14,14 +14,14 @@ const setupDatabase = () => {
   });
 
   const Artist = ArtistModel(sequelize, Sequelize);
-  const Album = AlbumModel(sequelize, Sequelize);
-  const Song = SongModel(sequelize, Sequelize);
+ // const Album = AlbumModel(sequelize, Sequelize);
+ // const Song = SongModel(sequelize, Sequelize);
 
   sequelize.sync({ alter: true });
   return {
     Artist,
-    Album,
-    Song,
+  //  Album,
+  //  Song,
   };
 };
 
