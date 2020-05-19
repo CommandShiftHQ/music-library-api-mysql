67896d78fc241bea990afd5b3017fec15f14f90f
ian-antking <ian.antking@gmail.com>
2020-05-19 09:03:35 +0100

    change sequelize to connection

--- src/models/index.js
+++ src/models/index.js
@@ -6,23 +6,23 @@ const SongModel = require('./song');
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
 const setupDatabase = () => {
-  const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     port: DB_PORT,
     dialect: 'mysql',
     logging: false,
   });
 
-  const Artist = ArtistModel(sequelize, Sequelize);
-  const Album = AlbumModel(sequelize, Sequelize);
-  const Song = SongModel(sequelize, Sequelize);
+  const Artist = ArtistModel(connection, Sequelize);
+  const Album = AlbumModel(connection, Sequelize);
+  const Song = SongModel(connection, Sequelize);
 
   Album.belongsTo(Artist, { as: 'artist' });
 
   Song.belongsTo(Artist, { as: 'artist' });
   Song.belongsTo(Album, { as: 'album' });
 
-  sequelize.sync({ alter: true });
+  connection.sync({ alter: true });
   return {
     Artist,
     Album,
